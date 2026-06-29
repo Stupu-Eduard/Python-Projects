747a4dfee42113c83c09f71ca8083759dd433cf7
Stupu Eduard-Stefan <eduard.stupu1@gmail.com>
2026-06-29 21:14:13 +0300

    Added yfinance to requirements and tested if it works

--- learning-python/001_equal_weight_S/first_program.py
+++ learning-python/001_equal_weight_S/first_program.py
@@ -1,8 +1,20 @@
 import numpy as np
 import pandas as pd
-import requests
+import yfinance as yf
 import xlsxwriter
 import math
 
 stocks = pd.read_csv('sp_500_stocks.csv')
-print(stocks);
+#print(stocks);
+
+symbol = 'AAPL'
+
+data = yf.Ticker(symbol).info
+
+#print(data)
+
+price = data['regularMarketPrice']
+#print(price)
+
+market_cap = data['marketCap']
+print(market_cap/1_000_000_000_000)
