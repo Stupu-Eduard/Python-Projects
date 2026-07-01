41fe6d5611be5ba360fbdd78b6333776a29bfe2d
Stupu Eduard-Stefan <eduard.stupu1@gmail.com>
2026-07-01 18:45:38 +0300

    Print first 10 Tickers with Stock Price, Market Capitalization, Number of Shares to buy

--- learning-python/001_equal_weight_SP500/first_program.py
+++ learning-python/001_equal_weight_SP500/first_program.py
@@ -6,6 +6,7 @@ import math
 
 stocks = pd.read_csv('sp_500_stocks.csv')
 #print(stocks);
+ 
 
 symbol = 'AAPL'
 
@@ -13,8 +14,36 @@ data = yf.Ticker(symbol).info
 
 #print(data)
 
+
 price = data['regularMarketPrice']
-#print(price)
+print(price)
+
 
 market_cap = data['marketCap']
-print(market_cap/1_000_000_000_000)
+#print(market_cap/1_000_000_000_000)
+
+
+my_columns = [ 'Ticker', 'Stock Price', 'Market Capitalization', 'Number of Shares to Buy' ]
+final_dataframe = pd.DataFrame(columns = my_columns)
+
+final_dataframe.loc[len(final_dataframe)] = [symbol, price, market_cap,'N/A']
+
+#print(final_dataframe.to_markdown())
+
+
+final_dataframe = pd.DataFrame(columns = my_columns)
+for stock in stocks['Ticker'][:10]:
+    #print(stock)
+
+    data = yf.Ticker(stock).info
+
+    price = data.get('currentPrice') or data.get('regularMarketPrice', 0.0)
+    m_cap = data.get('marketCap', 0)
+    
+    final_dataframe.loc[len(final_dataframe)] = [ 
+                                                 stock, 
+                                                 price, 
+                                                 m_cap, 
+                                                 'N/A' 
+                                                ]
+print(final_dataframe.to_markdown())
